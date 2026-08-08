d21251143a29698b0aec2fc44b9bc7578bbe2bcb
kushagratyagii <143875963+kushagratyagii@users.noreply.github.com>
2026-08-09 00:43:14 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS,COLS = len(board),len(board[0])
+        def connected(r,c):
+            if r<0 or r>= ROWS or c<0 or c>=COLS or board[r][c]!='O':
+                return
+            board[r][c] = 'T'
+            connected(r+1,c)
+            connected(r-1,c)
+            connected(r,c+1)
+            connected(r,c-1)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r in [0,ROWS-1] or c in [0,COLS-1]) and board[r][c]=='O':
+                    connected(r,c)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+
+        
